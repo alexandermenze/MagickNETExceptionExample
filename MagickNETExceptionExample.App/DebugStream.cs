775bf7d3f68c106035f8cfa839b5f8e2b893ca32
Alexander Menze <a.menze@polipol.de>
2022-08-04 15:00:00 +0200

    Outputting process information

---
+++ MagickNETExceptionExample.App/DebugStream.cs
@@ -0,0 +1,54 @@
+﻿namespace MagickNETExceptionExample.App
+{
+    public class DebugStream : Stream
+    {
+        private readonly Stream _innerStream;
+        private long _read = 0;
+        private int _seekCount = 0;
+
+        public override bool CanRead => _innerStream.CanRead;
+
+        public override bool CanSeek => _innerStream.CanSeek;
+
+        public override bool CanWrite => _innerStream.CanWrite;
+
+        public override long Length => _innerStream.Length;
+
+        public long ReadBytes => _read;
+        public long SeekCount => _seekCount;
+
+        public override long Position
+        {
+            get => _innerStream.Position;
+            set => _innerStream.Position = value;
+        }
+
+        public DebugStream(Stream innerStream)
+        {
+            _innerStream = innerStream;
+        }
+
+        public override void Flush()
+            => _innerStream.Flush();
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            var bytesRead = _innerStream.Read(buffer, offset, count);
+            _read += bytesRead;
+            return bytesRead;
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            var pos = _innerStream.Seek(offset, origin);
+            _seekCount++;
+            return pos;
+        }
+
+        public override void SetLength(long value) => 
+            _innerStream.SetLength(value);
+
+        public override void Write(byte[] buffer, int offset, int count) => 
+            _innerStream.Write(buffer, offset, count);
+    }
+}
--- MagickNETExceptionExample.App/Program.cs
+++ MagickNETExceptionExample.App/Program.cs
@@ -1,1 +1,57 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using ImageMagick;
+using MagickNETExceptionExample.App;
+using MetadataExtractor;
+using System.Diagnostics;
+
+while (true)
+{
+    Console.WriteLine();
+    Console.WriteLine();
+
+    Console.Write("Enter file path: ");
+    var filePath = Console.ReadLine();
+
+    if (filePath is null)
+    {
+        Console.WriteLine("Invalid filePath!");
+        continue;
+    }
+
+    Console.WriteLine($"Memory usage before: {CurrentMemoryUsageMb()}Mb");
+
+    try
+    {
+        var stopwatch = Stopwatch.StartNew();
+
+        using (var magickDebugStream = new DebugStream(new FileStream(filePath, FileMode.Open, FileAccess.Read)))
+        {
+            using var image = new MagickImage();
+            image.Ping(magickDebugStream);
+            Console.WriteLine($"Done pinging image with magick in {stopwatch.Elapsed}. Stream length: {magickDebugStream.Length}, bytes read: {magickDebugStream.ReadBytes}, seek count: {magickDebugStream.SeekCount}, memory usage: {CurrentMemoryUsageMb()}Mb");
+        }
+
+        Console.WriteLine($"Memory usage in between: {CurrentMemoryUsageMb()}Mb");
+
+        stopwatch.Restart();
+
+        using (var readerDebugStream = new DebugStream(new FileStream(filePath, FileMode.Open, FileAccess.Read)))
+        {
+            var metadata = ImageMetadataReader.ReadMetadata(readerDebugStream);
+            Console.WriteLine($"Done reading metadata in {stopwatch.Elapsed}. Stream length: {readerDebugStream.Length}, bytes read: {readerDebugStream.ReadBytes}, seek count: {readerDebugStream.SeekCount}, memory usage: {CurrentMemoryUsageMb()}Mb");
+        }
+
+        Console.WriteLine($"Memory usage after: {CurrentMemoryUsageMb()}Mb");
+
+        GC.Collect();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error reading file: {ex.Message}");
+    }
+}
+
+static double CurrentMemoryUsageMb()
+{
+    using var process = Process.GetCurrentProcess();
+    return process.PrivateMemorySize64 / (1024 * 1024);
+}
